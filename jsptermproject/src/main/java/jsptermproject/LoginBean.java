a7b1b99e1e375c585dfcca23ef8a9597ee1f34e4
Haneol Choi <ulywooly@gmail.com>
2022-06-03 03:44:15 +0900

    0603

---
+++ jsptermproject/src/main/java/jsptermproject/LoginBean.java
@@ -0,0 +1,44 @@
+package jsptermproject;
+
+public class LoginBean {
+	private String userID;
+	private String userPassword;
+	
+	final String _userID = "admin";
+	final String _userPassword = "1234";	
+	
+	public boolean checkUser() {
+		if(userID.equals(_userID) && userPassword.equals(_userPassword)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String get_userID() {
+		return _userID;
+	}
+
+	public String get_userPassword() {
+		return _userPassword;
+	}
+	
+
+}
---
+++ jsptermproject/src/main/webapp/login.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<jsp:useBean id="login" class="jsptermproject.LoginBean" scope="page" />
+<jsp:setProperty name="login" property="*" />
+    
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Page</title>
+</head>
+<body>
+<%
+	request.setCharacterEncoding("utf-8");
+
+	if(login.checkUser() == true){	
+%>
+	<script>
+		alert('로그인 성공');
+		location.href="index.jsp"
+	</script>
+<%
+	}
+	else{
+%>
+	<script>
+		alert('아이디 또는 패스워드를 확인하세요');
+		history.go(-1);
+	</script>
+<%
+	}
+%>
+
+</body>
